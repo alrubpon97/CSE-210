4d616952dbc8abeed3c09d13a62a6927b75063f3
alrubpon97 <jrubioponce@byupathway.edu>
2026-01-04 12:01:53 -0600

    Implement grade input and evaluation logic in Program.cs

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,64 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What was your grade? ");
+        string user_grade = Console.ReadLine();
+        int user_grade_int = int.Parse(user_grade);
+
+        string letter = "";
+
+        if (user_grade_int >= 90)
+        {
+            letter = "A";
+
+        }
+        else if (user_grade_int >= 80)
+        {
+            letter = "B";
+        }
+        else if (user_grade_int >= 70)
+        {
+            letter = "C";
+        }
+        else if (user_grade_int >= 60) {
+            letter = "D";
+        } 
+        else {
+            letter = "F";
+        }
+
+        string sign = "";
+        int lastDigit = user_grade_int % 10;
+
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+        if (letter == "A" && sign == "+")
+        {
+            sign = "";
+        }
+        else if (letter == "F")
+        {
+            sign = "";
+        }
+
+        Console.Write($"Your grade is {letter}{sign}.");
+        if (user_grade_int >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
