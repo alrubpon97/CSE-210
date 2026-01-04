ebaa508bdd29eb23014298df1b73d668a54b0180
alrubpon97 <jrubioponce@byupathway.edu>
2026-01-04 16:15:11 -0600

    Implement magic number guessing game logic in Program.cs

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // The user assigns the magic number
+        Console.WriteLine("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        int guess = 0;
+
+        while (guess != magicNumber)
+        {
+             // The user gueses a number
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+            Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+            Console.WriteLine("Lower");
+            }
+            else
+            {
+            Console.WriteLine("You guessed it!");
+            }
+        } 
     }
 }
