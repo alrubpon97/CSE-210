93ceaa68b602b23d8a40b79a99ab30c3252ab8ea
alrubpon97 <jrubioponce@byupathway.edu>
2026-01-04 18:08:22 -0600

    Implement user input functions for name and favorite number in Program.cs

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        WelcomeMessage();
+
+        string userLegalName = UserName();
+        int userFavNumber = FavNumber();
+        int squareNum = SquareNumber(userFavNumber);
+
+        Console.WriteLine($"{userLegalName}, the square of your number is {squareNum}");
+    }
+
+    // First function: Displays a welcome message
+    static void WelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    // Second function: returns the user's legal name
+    static string UserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    // Third function: returns the user's favorite number
+    static int FavNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int a)
+    {
+        return a * a;
     }
 }
